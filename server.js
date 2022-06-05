af5ccb0468c3b7586c4fa4e1c9758223002838d0
RogerLi818 <jackliteng@yahoo.com>
2022-06-04 20:48:27 -0700

    add mySQL in server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express ();
@@ -7,6 +8,24 @@ const app = express ();
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+
+
+//connect to database
+const db=mysql.createConnection(
+    {
+        host:'localhost',
+        //Your MySQL username,
+        user:'root',
+        //Your MySQL password
+        password:'Bai#2zhong',
+        database:'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`,(err,rows)=>{
+    console.log(rows);
+});
 //Default response for any other request (Not Fund)
 app.use((rep,res)=>{
     res.status(404).end();
