c747c1646bbd00f8d6baf9d740ac4078d17e7a3a
RogerLi818 <jackliteng@yahoo.com>
2022-06-12 23:46:14 -0700

    add post

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -35,4 +35,27 @@ router.get('/voter/:id',(req,res)=>{
     });
 });
 
+router.post('/voter',({body},res)=>{
+    // Data validation
+    const errors=inputCheck(body,'first_name','last_name','email');
+    if(errors){
+        res.status(400).json({error:errors});
+        return;
+    }    
+    
+    const sql=`INSERT INTO voter (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    db.query(sql, params, (err,result)=>{
+        if(err){
+            res.status(400),json({err:err.message});
+            return;
+        }
+        res.json({
+            message:'success',
+            data:body
+        });
+    });
+});
+
 module.exports= router;
